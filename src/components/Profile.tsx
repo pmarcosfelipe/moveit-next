3e415d3d21e8284d06a8250116304bd0462b3f93
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-02-25 20:44:06 +0000

    complete leveling funcionality

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/pmarcosfelipe.png" alt="Marcos Felipe" />
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>Marcos Felipe</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import Head from 'next/head';
+
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 
-import Head from 'next/head';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -16,16 +18,18 @@ export default function Home() {
       </Head>
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 }
